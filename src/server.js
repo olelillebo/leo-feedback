ea10d252eba4611fdaebd70deae7d927d3f7c6cb
Ole <yuhyuh@gmail.com>
2018-09-22 13:42:33 +0200

    added oauth

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const jwtCheck = require('express-jwt');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+
+app.use('/', jwtCheck);
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8082);
+
